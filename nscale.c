6c916a479ff20b289826fefad45022a227d458b6
maxsond <vagus.eques@gmail.com>
2019-01-27 21:16:05 +0000

    Bugfixes + documentation + option handling

--- nscale.c
+++ nscale.c
@@ -5,33 +5,119 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
 #include "scale.h"
+
+const char *argp_program_version = "nscale 1.0";
+const char *argp_program_bug_address = "<vagus.eques@gmail.com>";
+
+static char doc[] = "NScale calculates an octave in an equal temperament scale with the given start note and number of tones.\n\n"
+"The start value is interpreted as a MIDI note value by default, but can optionally be interpreted as a frequency in Hz.";
+
+static char args_doc[] = "tones start";
+
+static struct argp_option options[] = {
+  {"freqency",  'f',  0,  0,  "Interpret the start value as a frequency in Hz"},
+  {"output", 'o', "FILE", 0, "Output to file instead of stdout"},
+  {0}
+};
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+  char* args[2];                /* arg1 & arg2 */
+  int midifreq;
+  char* output_file;
+};
+
+/* Parse a single option. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state){
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+
+  switch (key)
+    {
+    case 'f':
+      arguments->midifreq = 1;
+      break;
+    case 'o':
+      arguments->output_file = arg;
+      break;
+
+    case ARGP_KEY_ARG:
+      if (state->arg_num >= 2)
+        /* Too many arguments. */
+        argp_usage (state);
+
+      arguments->args[state->arg_num] = arg;
+      break;
+
+    case ARGP_KEY_END:
+      if (state->arg_num < 2)
+        /* Not enough arguments. */
+        argp_usage (state);
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char* argv[]){
 
-  if(argc != 3){
-    printf("Usage: nscale <notes> <midinote>\n");
-    return 1;
+  struct arguments arguments;
+
+  /* Default values. */
+  arguments.midifreq = 0;
+  arguments.output_file = "-";
+
+  /* Parse our arguments; every option seen by parse_opt will
+     be reflected in arguments. */
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+  int notes;
+  double start;
+
+  notes = atoi(arguments.args[0]);
+
+  start = atof(arguments.args[1]);
+
+  if(arguments.midifreq == 0){
+    if (start < 0){
+      printf("Error: cannot have negative MIDI notes!\n");
+      return 1;
+    }
+    else if(start > 127){
+      printf("Error: maximum MIDI note is 127\n");
+      return 1;
+    }
   }
-
-  int notes, midinote;
-
-  notes = atoi(argv[1]);
-
-  midinote = atoi(argv[2]);
-  if(midinote < 0){
-    printf("Error: cannot have negative MIDI notes!\n");
-    return 1;
-  }
-  if(midinote > 127){
-    printf("Error: maximum MIDI note is 127\n");
-    return 1;
+  else{
+    if(start < 20 || start > 10000){
+      printf("Error: please start at an audible frequency between 20 and 10,000\n");
+      return 1;
+    }
   }
 
   double scale[notes];
-  compute_scale(notes, midinote, 0, (double*)&scale);
-  for(int i=0; i < notes; i++){
-    printf("%f\n", scale[i]);
+  compute_scale(notes, start, arguments.midifreq, (double*)&scale);
+  if (arguments.output_file == "-") {
+    for(int i=0; i < notes; i++){
+      printf("%f\n", scale[i]);
+    }
+  }
+  else{
+    FILE* f = fopen(arguments.output_file, "w+");
+    for(int i=0; i < notes; i++){
+      fprintf(f, "%f\n", scale[i]);
+    }
+    fclose(f);
+    printf("Saved scale to %s\n", arguments.output_file);
   }
   return 0;
 }
--- scale.c
+++ scale.c
@@ -6,13 +6,13 @@ int compute_scale(int notes, double start, int midi_or_freq, double* scale){
   ratio = pow(2.0, 1.0/12.0);
   c5 = 220.0 * pow(ratio, 3.0);
   c0 = c5 * pow(0.5, 5);
-  ratio = pow(2.0, 1.0/notes);
   if(midi_or_freq){
     frequency = start;
   }
   else{
     frequency = c0 * pow(ratio, start);
   }
+  ratio = pow(2.0, 1.0/notes);
   for(int i = 0; i < notes; i++){
     scale[i] = frequency * pow(ratio, i);
   }
